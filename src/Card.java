025794836dcaff385fcddafa7d17dbedda1fdcb1
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-04-20 13:41:26 +0200

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+    private int Mana;
+    private int Health;
+    private double Dodge;
+
+    public Card() {
+        Mana = 0;
+        Health = 1;
+        Dodge = 0.3;
+    }
+}
